815ec19f27b920f6a00285d88414fb3436e1da9c
Jordan Vera <verawebdevelopment@gmail.com>
2023-10-11 20:11:59 -0500

    deployment changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './Components/App.jsx';
 import NotFound from './Components/NotFound.jsx';
 import './styles/app.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <Router>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/rockPaperScissor" element={<App />} />
       <Route path="/*" element={<NotFound />} />
     </Routes>
-  </BrowserRouter>
+  </Router>
 );
